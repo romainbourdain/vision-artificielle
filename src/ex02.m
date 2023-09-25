8f69e5791a000bc67ac82a56177d3d603991d650
Romain <romain.bourdain93@gmail.com>
2023-09-25 09:41:52 +0200

    ex02

--- src/ex02.m
+++ src/ex02.m
@@ -13,6 +13,14 @@ M = M_chapeau / norm(M_chapeau(3, 1:3));
 
 m_1 = M(1, 1:3)';
 m_2 = M(2, 1:3)';
+m_3 = M(3, 1:3)';
+
+u_0 = m_1' * m_3;
+v_0 = m_2' * m_3;
+alpha_v = sqrt(m_2' * m_2 - v_0^2);
+gamma = (m_1' * m_2 - u_0*v_0)/alpha_v;
+alpha_u = sqrt(m_1' * m_1 - gamma^2 - u_0^2);
+
 
 
 rmpath("data/ex02/");
