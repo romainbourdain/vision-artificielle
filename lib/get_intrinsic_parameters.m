c880a3b6b3aa2fd31e122ae22cd14fe097da9b50
Romain <romain.bourdain93@gmail.com>
2023-09-25 09:37:32 +0200

    refactoring

--- lib/get_intrinsic_parameters.m
+++ lib/get_intrinsic_parameters.m
@@ -1,7 +1,3 @@
 function [alpha_u, alpha_v, gamma, u_0, v_0] = get_intrinsic_parameters(intrinsic_matrix)
-    alpha_u = intrinsic_matrix(1, 1);
-    alpha_v = intrinsic_matrix(2, 2);
-    gamma = intrinsic_matrix(1, 2);
-    u_0 = intrinsic_matrix(1, 3);
-    v_0 = intrinsic_matrix(2, 3);
+
 end
--- main.m
+++ main.m
@@ -1,2 +1,18 @@
-addpath("src/");
-addpath("lib/");
+function main(exo)
+    addpath("src/");
+    addpath("lib/");
+
+    if ~exist('exo', 'var')
+        return;
+    end
+
+    switch exo
+        case 0
+            ex00;
+        case 1
+            ex01;
+        case 2
+            ex02;
+    end
+
+end
